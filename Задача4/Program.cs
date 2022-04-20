3fb06aa5cc9f85dbe7bf0b276ef6c9c2b414e0d6
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-20 12:28:59 +0300

    programm4 ready

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int prov(int num1)
+{
+    int kratnost = 0;
+    int result1 = num1 % 7;
+    int result2 = num1 % 23;
+    if (result1 == 0 && result2 == 0)
+    {
+        kratnost = 1;
+    }
+    else
+    {
+        kratnost = 0;
+    }
+    return kratnost;
+}
+
+int a = 161;
+
+int kratnost = prov(a);
+
+if (kratnost == 1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
