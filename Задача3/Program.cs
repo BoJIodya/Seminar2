542e56d5dddfebf1013829e4e87b7b4a0a98db10
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-20 11:37:20 +0300

    programm ready

--- Задача3/Program.cs
+++ Задача3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Remains(int num1, int num2)
+{
+    int result = (num1 % num2);
+    return result;
+}
+int a = 25;
+int b = 5;
+int Rem = Remains(a, b);
+
+if (Rem == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.Write("Некратно, остаток ");
+    Console.WriteLine(Rem);
+}
