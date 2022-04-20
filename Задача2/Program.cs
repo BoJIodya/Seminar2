26354aac74a7e374843c993f7e6ae7e4854e6135
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-20 10:51:26 +0300

    написал программу и протестировал

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 123;
+int a = (N / 10) % 10;
+
+if (N >= 100 && N <= 999)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Число не является трёхзначным");
+}
