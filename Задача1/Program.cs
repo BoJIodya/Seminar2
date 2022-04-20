614b3ae9d121b91150c8d1822b0e8bb77ec54539
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-20 10:42:18 +0300

    commit init

---
+++ Задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = new Random().Next(10, 100);
+int a = N / 10;
+int b = N % 10;
+
+System.Console.WriteLine($"a = {a} b = {b} N = {N}");
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+Console.WriteLine($"maximum = {max}");
