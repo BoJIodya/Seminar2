eef96c8456a772c3284dc89f07f74276d25e24b8
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-21 11:25:02 +0300

    задача решена

--- Задача8/Program.cs
+++ Задача8/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7;
+
+if (a >= 1 && a <= 7)
+{
+    if (a >= 1 && a <= 5)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня не существует");
+}
