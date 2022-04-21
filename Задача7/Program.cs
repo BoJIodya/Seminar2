58116622998ba68a19ccad051905c84f1a987cc1
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-21 10:00:38 +0300

    причесал блоксхему

--- Задача7/Program.cs
+++ Задача7/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 258713658;
+
+int Count = (int)Math.Log10(a) + 1; //преобразование логарифма в цельночисловой тип
+
+int delit = (int)Math.Pow(10, (Count-3));
+//Console.WriteLine(delit);
+
+int result = a / delit;
+
+int itog = result%10; //это покажет третью цифру числа
+Console.WriteLine(result);
+Console.WriteLine(itog);
+//Console.WriteLine("5^4 = " + Math.Pow(5, 4));
